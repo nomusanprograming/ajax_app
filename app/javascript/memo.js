f4ea1bee7abf97d787d2a8668e019fd34e094e6d
nomusanprograming <69701122+nomusanprograming@users.noreply.github.com>
2020-09-22 17:00:25 +0900

    メモ投稿機能のフロントエンド実装

---
+++ app/javascript/memo.js
@@ -0,0 +1,32 @@
+function memo() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const formData = new FormData(document.getElementById("form"));
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/posts", true);
+    XHR.responseType = "json";
+    XHR.send(formData);
+    XHR.onload = () => {
+      if(XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }
+      const item = XHR.response.post; // レスポンスデータ
+      const list = document.getElementById("list"); // 描画する親要素
+      const formText = document.getElementById("content"); // 投稿時入力フォームを空白に戻すため
+      const HTML = `
+        <div class="post" data-id=${item.id}>
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+          ${item.content}
+          </div>
+        </div>`;
+      list.insertAdjacentHTML("afterend", HTML);
+      formText.value = "";
+    };
+    e.preventDefault();
+  });
+}
+window.addEventListener("load", memo);
